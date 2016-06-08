482ff1bf0690e261b8cc42b82d2c72cbf04cb0ec
David Carter <dajocarter@gmail.com>
2016-06-07 22:29:31 -0400

    Configure gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,110 @@
+var gulp = require('gulp'),
+  $ = require('gulp-load-plugins')(),
+  browserSync = require('browser-sync').create(),
+  Metalsmith = require('./node_modules/metalsmith'),
+  branch = require('./node_modules/metalsmith-branch'),
+  collections = require('./node_modules/metalsmith-collections'),
+  ignore = require('./node_modules/metalsmith-ignore'),
+  inPlace = require('./node_modules/metalsmith-in-place'),
+  layouts = require('./node_modules/metalsmith-layouts'),
+  markdown = require('./node_modules/metalsmith-markdown'),
+  permalinks = require('./node_modules/metalsmith-permalinks');
+
+var AUTOPREFIXER_BROWSERS = [
+  'ie >= 10',
+  'ie_mob >= 10',
+  'ff >= 30',
+  'chrome >= 34',
+  'safari >= 7',
+  'opera >= 23',
+  'ios >= 7',
+  'android >= 4.4',
+  'bb >= 10'
+];
+
+gulp.task('img', function() {
+  return gulp.src(['src/img/*.{png,PNG,jpg,JPG,jpeg,JPEG,gif,GIF}'], {
+      base: '.'
+    })
+    .pipe($.newer('./src/img/'))
+    .pipe($.imagemin())
+    .pipe(gulp.dest('./build/img/'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('js', function() {
+  return gulp.src(['src/js/vendor/*.js', 'src/js/*.js'])
+    .pipe($.concat('master.js', {
+      newLine: ';'
+    }))
+    .pipe($.uglify(false))
+    //.pipe($.rename('master.min.js'))
+    .pipe(gulp.dest('./build/js'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('sass', function() {
+  return gulp.src('src/scss/master.scss')
+    .pipe($.sourcemaps.init())
+    .pipe($.sass({
+      outputStyle: 'expanded'
+    }).on('error', $.sass.logError))
+    .pipe($.autoprefixer({
+      browsers: AUTOPREFIXER_BROWSERS
+    }))
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest('build/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('metalsmith', function() {
+  Metalsmith(__dirname)
+  .source('src')
+  .destination('build')
+  .use(ignore([
+    'img/',
+    'js/',
+    'scss/'
+  ]))
+  .use(collections({
+    'posts': 'blog/*.md'
+  }))
+  .use(branch('**/*.md')
+    .use(markdown())
+    .use(permalinks({
+      relative: false,
+      linksets: [{
+        match: {
+          collection: 'posts'
+        },
+        pattern: 'blog/:title'
+      }]
+    }))
+    .use(layouts({
+      engine: 'handlebars',
+      default: 'default.hbs',
+      directory: 'templates',
+      partials: 'partials',
+      rename: true
+    }))
+  )
+  .build(function(err) {
+    if (err) console.log(err);
+  })
+});
+
+gulp.task('watch', function() {
+  gulp.watch(['src/**/*', 'templates/**/*.hbs', 'partials/**/*.hbs'],['metalsmith']).on('change', browserSync.reload);
+  gulp.watch(['src/img/*'], ['img']);
+  gulp.watch(['src/js/**/*.js'], ['js']);
+  gulp.watch(['src/scss/**/*.scss'], ['sass']);
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: 'build',
+    watchTask: true
+  });
+});
+
+gulp.task('default', ['metalsmith', 'browserSync', 'watch']);
