319214b77fd2489caee8d65f385ec23d0d75e8a3
David Carter <dajocarter@gmail.com>
2016-06-17 15:18:26 -0400

    Add equals helper

---
+++ helpers/eq.js
@@ -0,0 +1,24 @@
+/**
+ * Block helper that renders a block if `a` is **equal to** `b`.
+ * If an inverse block is specified it will be rendered when falsy.
+ * You may optionally use the `compare=""` hash argument for the
+ * second value.
+ *
+ * @name .eq
+ * @param {String} `a`
+ * @param {String} `b`
+ * @param {Object} `options` Handlebars provided options object
+ * @return {String} Block, or inverse block if specified and falsey.
+ * @block
+ * @api public
+ */
+module.exports = function(a, b, options) {
+  if (arguments.length === 2) {
+    options = b;
+    b = options.hash.compare;
+  }
+  if (a === b) {
+    return options.fn(this);
+  }
+  return options.inverse(this);
+};
