8b4fd2f81ece14fc2f5516f1a8978fed9d14952b
David Carter <dajocarter@gmail.com>
2016-06-18 11:50:50 -0400

    Format js

--- src/js/master.js
+++ src/js/master.js
@@ -1,3 +1,11 @@
-var header = document.getElementById("header");
-var headroom  = new Headroom(header);
-headroom.init(); 
+var init = {
+  headroom: function() {
+    var header = document.getElementById("header");
+    var headroom  = new Headroom(header);
+    return headroom.init();    
+  }
+};
+
+document.addEventListener('DOMContentLoaded', function(event) {
+  init.headroom();
+});
