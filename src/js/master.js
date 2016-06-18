405887bab695f634ca85b089bfd83a0e3cd15d0f
David Carter <dajocarter@gmail.com>
2016-06-18 11:51:23 -0400

    Add a circles function

--- src/js/master.js
+++ src/js/master.js
@@ -3,9 +3,18 @@ var init = {
     var header = document.getElementById("header");
     var headroom  = new Headroom(header);
     return headroom.init();    
+  },
+  perfectCircles: function() {
+    var socialIcons = document.querySelectorAll('.social a');
+    for (var i = 0; i < socialIcons.length; i++) {
+      var height = socialIcons[i].offsetHeight;
+      socialIcons[i].style.width = height + 'px';
+    }
+    return false;
   }
 };
 
 document.addEventListener('DOMContentLoaded', function(event) {
   init.headroom();
+  init.perfectCircles();
 });
