c9c9e01fda156acc165f24860fff59572f5e12a7
David Carter <dajocarter@gmail.com>
2016-06-29 17:21:45 -0400

    Add hyphenate helper

---
+++ helpers/hyphenate.js
@@ -0,0 +1,17 @@
+/**
+ * Replace spaces in a string with hyphens.
+ *
+ * ```handlebars
+ * {{hyphenate "foo bar baz qux"}}
+ * //=> "foo-bar-baz-qux"
+ * ```
+ * @param  {String} `str`
+ * @return {String}
+ * @api public
+ */
+
+module.exports = function(str) {
+  if (str && typeof str === 'string') {
+    return str.split(' ').join('-');
+  }
+};
