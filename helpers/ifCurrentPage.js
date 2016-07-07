f932e01b93297686f3e29881149ed93025eafe26
David Carter <dajocarter@gmail.com>
2016-07-07 17:45:51 -0400

    Add helper for current page

---
+++ helpers/ifCurrentPage.js
@@ -0,0 +1,20 @@
+/**
+ * Block helper that always renders the inverse block **unless `a` is
+ * is equal to `b`**.
+ *
+ * @name .unlessEq
+ * @param {String} `a`
+ * @param {String} `b`
+ * @param {Object} `options` Handlebars provided options object
+ * @return {String} Inverse block by default, or block if falsey.
+ * @block
+ * @api public
+ */
+
+module.exports = function(context, options) {
+  var path = context.split('/');
+  if ((path.length > 2 && path[1] == options.hash.compare) || (path.length == 2 && options.hash.compare == 1)) {;
+    return options.fn(this);
+  }
+  return options.inverse(this);
+};
