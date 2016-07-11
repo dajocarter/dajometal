c720c23ac046ae0fd5e3d38c9ab5aee0127de19e
David Carter <dajocarter@gmail.com>
2016-07-11 12:05:10 -0400

    Update URL

--- gulpfile.js
+++ gulpfile.js
@@ -119,7 +119,7 @@ gulp.task('metalsmith', function() {
       rename: true
     }))
     .use(sitemap({
-      hostname: 'https://dajometal.surge.sh'
+      hostname: 'https://app.netlify.com/sites/estates-officer-alerts-20653'
     }))
     .build(function(err) {
       if (err) console.log(err);
