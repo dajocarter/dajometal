aff5927862446f6e1275168db686ba227fad2d8d
David Carter <dajocarter@gmail.com>
2016-07-12 00:51:46 -0400

    Update URL

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ Metalsmith(__dirname)
     rename: true
   }))
   .use(sitemap({
-    hostname: 'https://dajometal.surge.sh'
+    hostname: 'http://dajometal.pubstorm.site'
   }))
   .build(function(err) {
     if (err) console.log(err);
